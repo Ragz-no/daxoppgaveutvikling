715670628eef24483ce028faaee27ff776a4999b
Ram Singh <70691805+Ragz-no@users.noreply.github.com>
2022-03-01 15:13:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // Set Item
 
-localStorage.setItem("inputText", "demo");
+localStorage.setItem("inputText", "velkomtekst");
 
 // Retrieve
 
 function daxOppgave() {
-    document.getElementById("demo").innerHTML = localStorage.getItem("inputText");
-    document.getElementById("demo").innerHTML = "Velkommen, vi ønsker deg alt godt " 
+    document.getElementById("velkomtekst").innerHTML = localStorage.getItem("inputText");
+    document.getElementById("velkomtekst").innerHTML = "Velkommen, vi ønsker deg alt godt " 
     + document.getElementById("inputText").value + "!";
     document.getElementById("inputText").value="";
 }
@@ -36,38 +36,7 @@ function takkeListe() {
 
 
     
-    
-
-
-
-    
-
-
-
-// TODO: få dette til funke firebase
-
-
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCNKLiVQm1gLTrbvyh9usOlSi4_9AQpAiA",
-//   authDomain: "daxoppgave1.firebaseapp.com",
-//   projectId: "daxoppgave1",
-//   storageBucket: "daxoppgave1.appspot.com",
-//   messagingSenderId: "760117309580",
-//   appId: "1:760117309580:web:683ad248b34c162db84ba9",
-//   measurementId: "G-VYVLNXPWQH"
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
+   
 
 
 
